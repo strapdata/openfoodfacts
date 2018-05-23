fe18f77d6057ee270e989d53e8dc16f0123b1c8a
dbarthe <barthelemy.delemotte@gmail.com>
2018-05-23 18:30:17 +0200

    add RF parameter

--- meetup/database.py
+++ meetup/database.py
@@ -5,13 +5,16 @@ import os
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cqlengine import connection
 from cassandra.cqlengine import management
+from cassandra.policies import DCAwareRoundRobinPolicy
 from elasticsearch import Elasticsearch
 
 from meetup.models import Product
 from meetup import RESOURCES_DIR
 
-keyspace = os.environ.get('ELASSANDRA_KEYSPACE', 'meetup')
 endpoints = os.environ.get('ELASSANDRA_ENDPOINTS', '127.0.0.1').split(',')
+
+keyspace = os.environ.get('ELASSANDRA_KEYSPACE', 'meetup')
+replication_factor = int(os.environ.get('ELASSANDRA_RF', 1))
 
 config_auth = None
 if 'ELASSANDRA_LOGIN' in os.environ:
@@ -19,13 +22,15 @@ if 'ELASSANDRA_LOGIN' in os.environ:
         'username': os.environ.get('ELASSANDRA_LOGIN'),
         'password': os.environ.get('ELASSANDRA_PASSWORD')
     }
+    print("using auth")
 else:
     config_auth = None
 
 if 'ELASSANDRA_CERTFILE' in os.environ:
     config_ssl = {
-        'cacert': 'ELASSANDRA_CERTFILE'
+        'cacert': os.environ.get('ELASSANDRA_CERTFILE')
     }
+    print("using ssl", config_ssl)
 else:
     config_ssl = None
 
@@ -66,10 +71,12 @@ def init():
                      default_keyspace=keyspace,
                      protocol_version=3,
                      auth_provider=auth_provider,
-                     ssl_options=ssl_options)
+                     ssl_options=ssl_options,
+                     load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='DC1'),
+                     retry_connect=True)
 
     # Synchronize cassandra schema
-    management.create_keyspace_network_topology(keyspace, {'DC1': 1})
+    management.create_keyspace_network_topology(keyspace, {'DC1': replication_factor})
     connection.execute(open(os.path.join(RESOURCES_DIR, 'schema.cql')).read())
     management.sync_table(Product, keyspaces=[keyspace])
 
